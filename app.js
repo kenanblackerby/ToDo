17ffa96ddf9b2e6f3da76159442497bdb1226e28
Kenan Blackerby <kblackerby2@gmail.com>
2020-11-22 23:47:04 -0600

    Complete basic functionality: Add to list, complete items, delete items, clear list

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
 var taskList = [{msg: 'This is not complete', complete: false}, {msg: 'this is complete', complete: true}];
 
+// Add new item to task list
 function addToTaskList() {
-
-    displayList(taskList);
+    const textArea = document.querySelector("#task");
+    const task = textArea.value;
+    textArea.value = '';
+    taskList.push({msg: task, complete: false});
+    displayTasks();
+    return false;
 }
 
+// Clear all tasks from list
 function clearList() {
+    taskList = [];
+    displayTasks();
 }
 
-function displayList() {
-    let listContent = "";
-    listContent = taskList.reduce((tasks, task) => {
-        if(task.complete === false) {
-            tasks +=  '<li>'
-                    +   '<p>' + task.msg + '</p>'
-                    +   '<button onclick="completeTask();">Check</button>'
-                    +   '<button onclick="deleteTask();">X</button>'
-                    + '</li>';
+// Mark task as complete
+function completeTask(taskIndex) {
+    taskList[taskIndex].complete = true;
+    displayTasks();
+}
+
+// Remove task from list
+function deleteTask(taskIndex) {
+    taskList.splice(taskIndex,1);
+    displayTasks();
+}
+
+// Display all incomplete tasks in list
+function displayTasks() {
+    let taskContent = '';
+    let completeContent = '';
+    
+    for (let index = 0; index < taskList.length; index++) {
+        const task = taskList[index];
+        if(!task.complete) {
+            taskContent +=  `<li>`
+                            +   `<p>${task.msg}</p>`
+                            +   `<button onclick="completeTask(${index});">Check</button>`
+                            +   `<button onclick="deleteTask(${index});">X</button>`
+                          + `</li>`;
+        } else {
+            completeContent +=  `<li>`
+                                +   `<p>${task.msg}</p>`
+                                +   `<button onclick="deleteTask(${index});">X</button>`
+                              + `</li>`;
         }
-        return tasks;
-    },'');
-    document.getElementById('task-list').innerHTML = listContent;
+    }
+    document.getElementById('task-list').innerHTML = taskContent;
+    document.getElementById('completed-list').innerHTML = completeContent;
 }
 
-function completeTask() {
-}
-
-function deleteTask() {
-}
-
-displayList(taskList);
+displayTasks();
