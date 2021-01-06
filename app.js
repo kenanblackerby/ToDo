057ddfbe71fba74b0a18c690032b7f614f19374d
Kenan Blackerby <kblackerby2@gmail.com>
2021-01-05 18:46:42 -0600

    Add localStorage functionality

--- app.js
+++ app.js
@@ -8,52 +8,36 @@ const filter = document.querySelector('#filter-todo');
 
 // Event Listeners
 addButton.addEventListener('click', addTodo);
-// todoInput.addEventListener('beforeinput', e => {
-//     if (e.data === null) {
-//         e.preventDefault();
-//         addButton.click();
-//     }
-// });
 clearButton.addEventListener('click', clearList);
 filter.addEventListener('click', filterTodos);
+document.addEventListener('DOMContentLoaded', getStoredTodos);
 
 // Functions
 
 // Add new item to task list
-function addTodo() {
-    // Todo DIV
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todo');
-    // Create LI
-    const newTodo = document.createElement('li');
-    newTodo.innerHTML = todoInput.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
-    // Checkmark BUTTON
-    const completeBtn = document.createElement('button');
-    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
-    completeBtn.classList.add('complete-btn');
-    completeBtn.addEventListener('click', checkItem);
-    todoDiv.appendChild(completeBtn);
-    // Delete BUTTON
-    const deleteBtn = document.createElement('button');
-    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.addEventListener('click', deleteItem);
-    todoDiv.appendChild(deleteBtn);
+function addTodo(e) {
+    e.preventDefault();
     // Add Todo to list
-    todoList.appendChild(todoDiv);
+    todoList.appendChild(constructTodoItem(todoInput.value));
+    // Save to LocalStorage
+    savelocalTodo(todoInput.value);
     // Clear Todo Text Area
     todoInput.value = '';
 }
 
 // Clear all tasks from list
 function clearList() {
+    todoList.innerHTML = '';
+    localStorage.removeItem('todos');
 }
 
 function deleteItem(e) {
-    const item = e.currentTarget.parentElement;
-    item.remove();
+    const itemEl = e.currentTarget.parentElement;
+    const todo = itemEl.children[0].innerText;
+    let todos = JSON.parse(localStorage.getItem('todos'));
+    todos.splice(todos.indexOf(todo), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    itemEl.remove();
 }
 
 function checkItem(e) {
@@ -63,7 +47,7 @@ function checkItem(e) {
 
 function filterTodos(e) {
     const todos = todoList.childNodes;
-    todos.forEach(function(todo) { 
+    todos.forEach(todo => { 
         switch(e.target.value) {
             case 'all':
                 todo.classList.remove('hidden');
@@ -84,4 +68,48 @@ function filterTodos(e) {
                 break;
         }
     })
+}
+
+function savelocalTodo (todo) {
+    let todos = [];
+    if (localStorage.getItem('todos') !== null) {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getStoredTodos () {
+    let todos = [];
+    if (localStorage.getItem('todos') !== null) {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(todo => {
+        todoList.appendChild(constructTodoItem(todo));
+    })
+}
+
+function constructTodoItem (todoText) {
+    // Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerHTML = todoText;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Checkmark BUTTON
+    const completeBtn = document.createElement('button');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+    completeBtn.classList.add('complete-btn');
+    completeBtn.addEventListener('click', checkItem);
+    todoDiv.appendChild(completeBtn);
+    // Delete BUTTON
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', deleteItem);
+    todoDiv.appendChild(deleteBtn);
+    
+    return todoDiv;
 }
